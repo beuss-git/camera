945d9379895de1d87a57694d95fde63248e3baa3
beuss <beuss.code@gmail.com>
2024-03-09 15:34:19 +0100

    Formatting

--- src/Platform.h
+++ src/Platform.h
@@ -3,16 +3,14 @@
  *
  * Copyright 2021 Arducam Technology co., Ltd. All Rights Reserved.
  *
- * This work is licensed under the MIT license, see the file LICENSE for details.
+ * This work is licensed under the MIT license, see the file LICENSE for
+ * details.
  *
  */
 
-
-#ifndef  __PLATFORM_H
-#define  __PLATFORM_H
-
+#ifndef __PLATFORM_H
+#define __PLATFORM_H
 
 #include "raspberryHal.h"
 
 #endif /*__PLATFORM_H*/
-
